0724104480e4a5ba8609c20bda20c73e2e41e1ff
root <root@LAPTOP-02N770GK>
2024-08-05 00:18:06 +0700

    MEET2 3:55:45

--- ini_module/models/planning_role.py
+++ ini_module/models/planning_role.py
@@ -12,6 +12,24 @@ class PlanningRoleTraining(models.Model):
     name = fields.Char('Nama Role', index=True)
     active = fields.Boolean('Active', default=True)
     color = fields.Integer('Color', default=_get_default_color)
+    company_id = fields.Many2one('res.company', # Model Tujuan
+                                 string='Company', # Label untuk field ini
+                                 default=lambda self : self.env.company) # Nilai default menggunakan lambda
+    
+    currency_id = fields.Many2one('res.currency', # Model Tujuan
+                                  string='Currency',  # Label untuk field ini
+                                  related='company_id.currency_id', # Field ini terkait dengan field currency_id dari model company_id
+                                  store=True) # Nilai field ini akan disimpan dalam basis data, bukan secara dinamis
+    
+    point_rate = fields.Integer('Point Rate')
+    amount = fields.Monetary('Amount', currency_id='currency_id')
+    resource_ids = fields.Many2many(comodel_name ='resource.resource', # Model tujuan
+                                    relation ='plannig_resource_rel', # Nama tabel relasi di database
+                                    column1 ='planning_role_id', # Nama field di tabel relasi yang merujuk ke model saat ini
+                                    column2 ='resource_id', # Nama kolom di tabel relasi yang merujuk ke model tujuan
+                                    string='Resource') # Label untuk field di UI
+    sequence = fields.Integer('Sequence')
+
 
 
 #_rec_name =
